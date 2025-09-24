f638f3e5b9d5ffedd3be28206385155ca9d846bd
Hirakamu <achmadirsyadhamizan@gmail.com>
2025-09-24 23:53:24 +0700

    new system

---
+++ wikiv2_wordpress/templates/admin_login.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name: Wiki Admin Login
+*/
+get_header();
+?>
+<main>
+  <h1>Admin Login</h1>
+  <form>
+    <!-- Add admin login form fields -->
+  </form>
+</main>
+<?php
+get_footer();
---
+++ wikiv2_wordpress/templates/index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Template Name: Wiki Index
+*/
+get_header();
+?>
+<main>
+  <h1>Welcome to Wikiv2 (WordPress)</h1>
+  <p>This is the WordPress version of the wiki index page.</p>
+</main>
+<?php
+get_footer();
